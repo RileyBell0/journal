d43fce7349d225ed079406d6f76d2b334ba12e66
Riley Bell <56055271+RileyBell0@users.noreply.github.com>
2023-06-26 17:31:43 +1000

    configure sever to run on https

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import fs from 'fs';
 
 export default defineConfig({
+	server: {
+		https: {
+			key: fs.readFileSync('/etc/ssl/server.key'),
+			cert: fs.readFileSync('/etc/ssl/server.crt')
+		},
+		port: 3000
+	},
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
