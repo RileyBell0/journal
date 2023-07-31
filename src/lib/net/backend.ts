7f8735bb344d52c24aa014db7eb2cf0f9acaf146
Riley Bell <56055271+RileyBell0@users.noreply.github.com>
2023-07-31 20:14:27 +1000

    Rename `devrileydev` to `dev`

--- src/lib/net/backend.ts
+++ src/lib/net/backend.ts
@@ -1,11 +1,11 @@
 import axios, { type AxiosInstance } from 'axios';
 
 const backend: AxiosInstance = axios.create({
-	baseURL: 'https://devrileydev.com/api',
-	headers: {
-		withCredentials: true,
-		secure: true
-	}
+    baseURL: 'https://dev.com/api',
+    headers: {
+        withCredentials: true,
+        secure: true
+    }
 });
 
 export default backend;
--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import { defineConfig } from 'vitest/config';
 import fs from 'fs';
 
 export default defineConfig({
-	server: {
-		https: {
-			key: fs.readFileSync('/etc/ssl/server.key'),
-			cert: fs.readFileSync('/etc/ssl/server.crt')
-		},
-		port: 3000
-	},
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+    server: {
+        https: {
+            key: fs.readFileSync('/etc/ssl/server.key'),
+            cert: fs.readFileSync('/etc/ssl/server.crt')
+        },
+        port: 3000
+    },
+    plugins: [sveltekit()],
+    test: {
+        include: ['src/**/*.{test,spec}.{js,ts}']
+    }
 });
